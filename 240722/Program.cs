0a6e3abd10cbe93604e59ff4b5b0e7c2da1919a7
gojjang10 <gojjang2@gmail.com>
2024-07-22 15:40:17 +0900

    OOP
    객체지향과 클래스 1번 과제

---
+++ 240722/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Chracter
+    {
+        int level;
+        int hp;
+        int speed;
+        int playerStr;
+
+        void GoStraight()
+        {
+
+        }
+
+        void GoBack()
+        {
+
+        }
+
+        void TurnLeft()
+        {
+
+        }
+
+        void TurnRight()
+        {
+
+        }
+
+        void Attack()
+        {
+
+        }
+
+        void Hit()
+        {
+
+        }
+    }
+}
